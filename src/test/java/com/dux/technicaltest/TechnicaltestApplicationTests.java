2ad5b6e74c5699d86c222b5f07260ca306b61472
lisandrofunes <lisandrofunes05@gmail.com>
2025-04-29 01:27:04 -0300

    initial commit

---
+++ src/test/java/com/dux/technicaltest/TechnicaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dux.technicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicaltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
